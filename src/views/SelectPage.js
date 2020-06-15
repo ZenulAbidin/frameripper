3ec50f880bd38105dc9251c5f91b48fe473f1fca
Ali Sherief <alihsherief@linuxmail.org>
2020-06-16 00:09:08 +0200

    save files

--- src/views/SelectPage.js
+++ src/views/SelectPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css";
+import "../assets/css/styles.css";
 import {Link} from "react-router-dom";
 import {Form, FormGroup, Button, Input, Label, Tooltip} from "reactstrap";
 
--- src/views/SettingsPage.js
+++ src/views/SettingsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {Form, FormGroup, Button, Col, Input, Label, Tooltip} from "reactstrap";
-import "./styles.css";
+import "../assets/css/styles.css";
 
 var process = require('process');
 
