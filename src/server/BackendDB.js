630357d44c62f61a294451f53098ec1e217d1fb8
Ali Sherief <alihsherief@linuxmail.org>
2020-06-17 10:40:29 +0200

    fix syntax error

--- src/server/BackendDB.js
+++ src/server/BackendDB.js
@@ -14,7 +14,7 @@ const DBfile = path.join(homedir, ".frameripper", "frameripper.db");
 const logfile = path.join(homedir, ".frameripper", `frameripper_${moment().format('YYYY-MM-DD-HH-mm-ss')}.log`);
 
 //const logger = pino({name: 'frameripper', level: 'trace'}, pino.destination({dest: logfile, minLength: 4096, sync: true}));
-const logger = pino(name: 'frameripper', level: 'trace'}, pino.destination());
+const logger = pino({name: 'frameripper', level: 'trace'}, pino.destination());
 
 var JPGcomplete = false;
 var PNGcomplete = false;
