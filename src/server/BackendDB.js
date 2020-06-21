c630b242192df3c637abc27fd73990cdda57542b
Ali Sherief <alihsherief@linuxmail.org>
2020-06-22 01:18:04 +0200

    test files

--- src/server/BackendDB.js
+++ src/server/BackendDB.js
@@ -508,16 +508,15 @@ const setNumFrames = (db, project, numFrames) => {
 const getFramesList = (db, project) => {
   logger.debug({time: moment().format("YYYY-MM-DDTHH:mm:ss.SSSSSSSSSZ"), app_subsystem: 'function_call', app_func: 'const getFramesList = (db, project) => {', app_file: '/server/BackendDB.js'});
   return new Promise((resolve, reject) => {
-    var exists = project != null && getProjects(db).then(projects => {
-      console.log(`exists = ${projects.includes(project)}`)
-      return projects.includes(project);
+    getProjects(db).then(projects => {
+      var exists = project != null && projects.includes(project);
+      if (!exists) {
+        logger.error({time: moment().format("YYYY-MM-DDTHH:mm:ss.SSSSSSSSSZ"), app_subsystem: 'database', app_request: 'get', app_key: '/project/'+project+'/framesList', app_response: {success: false, 'error': 'Project doesn\'t exist'}});
+        reject('Project doesn\'t exist');
+      }
     }).catch(err => {
       reject(err);
     });
-    if (!exists) {
-      logger.error({time: moment().format("YYYY-MM-DDTHH:mm:ss.SSSSSSSSSZ"), app_subsystem: 'database', app_request: 'get', app_key: '/project/'+project+'/framesList', app_response: {success: false, 'error': 'Project doesn\'t exist'}});
-      reject('Project doesn\'t exist');
-    }
     var framesList = db.get('/project/'+project+'/framesList').then(value => {
       return value;
     }).catch(err => {
