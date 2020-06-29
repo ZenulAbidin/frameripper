671d02d91bc97291f9d6c06d69627fbc1c9c7d0b
Ali Sherief <alihsherief@linuxmail.org>
2020-06-29 05:01:48 +0200

    fix bugs

--- src/views/SelectedComponents.js
+++ src/views/SelectedComponents.js
@@ -59,7 +59,7 @@ class SelectedComponent extends React.Component {
     const childrenWithProps = Children.map(this.props.children, child => {
       // Checking isValidElement is the safe way and avoids a TS error too.
       if (isValidElement(child)) {
-        return cloneElement(child, { onClick=this.handleOnClick })
+        return cloneElement(child, { onClick: this.handleOnClick })
       }
       return child;
     });
