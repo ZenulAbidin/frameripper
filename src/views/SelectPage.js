bd10790488c9bffc9155106cf5a4b41f231c4769
Ali Sherief <alihsherief@linuxmail.org>
2020-06-30 21:29:07 +0200

    add margin left to selectPage

--- src/views/SelectPage.js
+++ src/views/SelectPage.js
@@ -167,7 +167,7 @@ class SelectPage extends React.Component {
       <>
         <h1 className='title'>Frameripper by Zenul_Abidin</h1>
         <h3 style={{textAlign: 'center'}}>Select frames for {this.state.project}</h3>
-        <div>
+        <div {{marginLeft: '1rem'}}>
           <Form>
             <FormGroup>
               <Label for="inputFrameNumbers" id="frameNumbersToolTip">Frame numbers</Label>
