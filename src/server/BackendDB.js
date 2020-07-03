a418a88220e16fb849a21c21574fc3cd21c62bf3
Ali Sherief <alihsherief@linuxmail.org>
2020-07-03 04:04:09 +0200

    tapped wrong endpoint

--- src/server/BackendDB.js
+++ src/server/BackendDB.js
@@ -148,6 +148,7 @@ app.post('/projects', function (req, res) {
 })
 
 app.get('/currentproject', function (req, res) {
+  console.log(req)
   logger.debug({time: moment().format("YYYY-MM-DDTHH:mm:ss.SSSSSSSSSZ"), app_subsystem: 'function_call', app_func: 'app.get(\'/currentproject\', function (req, res) {', app_file: '/server/BackendDB.js'});
   if (!argv.testClient) {
     getCurrentProject(db).then(function(project) {
@@ -165,7 +166,6 @@ app.get('/currentproject', function (req, res) {
 })
 
 app.post('/currentproject', function (req, res) {
-  console.log(req)
   logger.debug({time: moment().format("YYYY-MM-DDTHH:mm:ss.SSSSSSSSSZ"), app_subsystem: 'function_call', app_func: 'app.post(\'/currentproject\', function (req, res) {', app_file: '/server/BackendDB.js'});
   if (!argv.testClient) {
     if (req.body.currentProject == null) {
