c0a676e34265df185c6dc7d20ddabe36763f42b3
Ali Sherief <alihsherief@linuxmail.org>
2020-07-04 02:55:37 +0200

    remove extraneous button tooltip

--- src/views/SelectPage.js
+++ src/views/SelectPage.js
@@ -300,15 +300,15 @@ class SelectPage extends React.Component {
           <div className='centered-horz'>
             <Form>
               <FormGroup row style={{marginRight: '1rem'}}>
-                <Label for="serverAddress" id="saveTooltip">API server address</Label>
+                <Label for="serverAddress" id="addressTooltip">API server address</Label>
                 <Input type="text" id="serverAddress" onChange={e => this.setServerAddress(e)} onKeyPress={(t) => {if (t.charCode===13) {this.commitServerAddress()}}}
                     value={this.state.serverAddress}/>
-                 <Button id="saveTooltip" color="primary" onClick={this.commitServerAddress}>Save address</Button>
+                 <Button color="primary" onClick={this.commitServerAddress}>Save address</Button>
               </FormGroup>
             </Form>
           </div>
         </div>
-        <Tooltip placement="bottom" isOpen={this.state.addressTooltipOpen} target="saveTooltip" toggle={this.toggleAddressTooltipOpen}>
+        <Tooltip placement="bottom" isOpen={this.state.addressTooltipOpen} target="addressTooltip" toggle={this.toggleAddressTooltipOpen}>
           Sets the address of the API server to send queries to. It can be an IP address or a domain name and a path. port number can also be specified. Prepend {'http://'} or {'https://'} to it and don&apos;t end it with a slash.
         </Tooltip>
       </>
